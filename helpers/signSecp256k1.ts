de684568422fedca21594f409988b84138397ffa
Julien Fraichot <julien.fraichot@hyland.com>
2022-08-22 14:17:17 +0200

    generate keys to issuer profile

--- helpers/signSecp256k1.ts
+++ helpers/signSecp256k1.ts
@@ -1,9 +1,12 @@
+import writeFile from "./writeFile";
+
 const { EcdsaSecp256k1Signature2019 } = require('@bloomprotocol/ecdsa-secp256k1-signature-2019');
 const jsigs = require('jsonld-signatures');
 const {purposes: {AssertionProofPurpose}} = jsigs;
 import generateSignerData, {SupportedSuites} from './generateSignerData';
 import currentTime from './currentTime';
 import generateDocumentLoader from './generateDocumentLoader';
+import issuerProfile from '../identity/issuer-blockcerts.json';
 
 export default async function signSecp256k1 (credential, keyPair = null, didDocument = null, documentLoader = generateDocumentLoader()) {
   if (!keyPair) {
@@ -12,14 +15,23 @@ export default async function signSecp256k1 (credential, keyPair = null, didDocu
     keyPair = signerData.keyPair;
     console.log('before sign', keyPair);
     didDocument = signerData.didDocument;
-    credential.issuer = didDocument.id;
+    credential.issuer = issuerProfile.id;
+    const keyId = 'secp256k1-verification-public-key'
+    keyPair.id = `${issuerProfile.id}#${keyId}`;
+    keyPair.controller = issuerProfile.id;
+    const verificationMethod = JSON.parse(JSON.stringify(didDocument.verificationMethod[0]))
+    verificationMethod.id = keyId;
+    verificationMethod.controller = issuerProfile.id;
+    issuerProfile.verificationMethod.push(verificationMethod);
+
+    await writeFile(issuerProfile, 'issuer-blockcerts.json', 'identity/');
   }
   const suite = new EcdsaSecp256k1Signature2019({ key: keyPair });
   suite.date = currentTime();
 
   const signedCredential = await jsigs.sign(credential, {
     suite,
-    purpose: new AssertionProofPurpose({ controller: didDocument }),
+    purpose: new AssertionProofPurpose({ controller: issuerProfile }),
     documentLoader
   });
   console.log('credential signed', signedCredential.proof);
